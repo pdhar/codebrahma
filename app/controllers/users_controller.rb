796e9758a35dd0aa06284cef6af19e6a1c57d343
pdhar <pranav.dhar2@gmail.com>
2013-09-25 13:10:27 +0530

    added db seed to add test users and messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,15 +2,13 @@ class UsersController < ApplicationController
   before_filter :signed_in_user, only: [:index]
   respond_to :json
   def index
-    #@user = User.find(:all, :conditions => ['id not in (?)', current_user]) 
-    @user = User.all
+    @user = User.find(:all, :conditions => ['id not in (?)', current_user]) 
     render :json => @user.to_json
   end
     
   def show
     @user = User.find(params[:id])
     render :json => @user.to_json(:include => :feeds) 
-    
   end
   
   def update  
